0d5859949b8584ebb55b0ab31ade83deebcde9e3
AnjasFedo <fedoaf98@gmail.com>
2024-06-10 17:26:03 +0700

    Do// The Migration

--- database/migrations/2024_06_10_101216_create_articles_table.php
+++ database/migrations/2024_06_10_101216_create_articles_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('description');
             $table->string('body');
             $table->timestamps();
+
+            $table->index('slug');
         });
     }
 
